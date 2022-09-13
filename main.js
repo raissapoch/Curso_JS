ad60f48e57be81f75698923e45d361544ada138a
Raissa Poch <raissa.poch@gmail.com>
2022-09-13 15:54:16 +0100

    condições e operadores lógicos

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for(let contador = 0; contador < ListaDeTeclas.length; contador++){
+
+    const tecla = ListaDeTeclas[contador];
+
+    const instrumento = tecla.classList[1];
+
+    //template string
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        
+        tocaSom(idAudio)
+    }
+
+    tecla.onkeydown = function(evento) {
+
+        if(evento.code === 'space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function() {
+        tecla.classList.remove('remove');
+    }
+}
 
